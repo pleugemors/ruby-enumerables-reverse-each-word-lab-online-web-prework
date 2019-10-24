750ca67e8bf97079bb99d37b8a6fa22801d48b60
Paul Leugemors <pleugemors@yahoo.com>
2019-10-24 08:42:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,13 @@ def reverse_each_word(string_sentence)
   puts string_sentence
   array_new = string_sentence.split(" ")
   c = array_new.collect{|ind| ind.reverse}
+<<<<<<< HEAD
   b= c.join(" ")
   b
+=======
+   puts c 
+  c.join(" ")
+  puts c
+  c
+>>>>>>> cd1be53e8251c094e42aaca5b1dc37ae08b9cd20
 end
